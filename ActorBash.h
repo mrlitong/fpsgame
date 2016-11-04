0f1c59731469f364cf1ef6766b8ca397566c5de2
mrlitong <litongtongxue@gmail.com>
2016-11-04 09:47:59 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

---
+++ ActorBash.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef __ACTOR_BASE_H__
+#define __ACTOR_BASE_H__
+
+#include "Shape.h"
+#include "Bounds.h"
+
+class CActorBase
+{    
+	CActorBase();
+	virtual ~CActorBase();
+
+};
