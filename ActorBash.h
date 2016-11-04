db8d1b162598e0593d4d929a06f5f5140bba4768
mrlitong <litongtongxue@gmail.com>
2016-11-04 09:51:27 +0800

    状态

--- ActorBash.h
+++ ActorBash.h
@@ -10,4 +10,25 @@ class CActorBase
 	CActorBase();
 	virtual ~CActorBase();
 
+	void SetEnabled(int enable);
+	int IsEnabled() const;
+
+	void Update(float ifps);
+
+	// intersection mask
+	void SetIntersectionMask(int mask);
+	int GetIntersectionMask() const;
+
+	enum {            //×´Ì¬
+		STATE_FORWARD = 0,
+		STATE_BACKWARD,
+		STATE_MOVE_LEFT,
+		STATE_MOVE_RIGHT,
+		STATE_CROUCH,	//¶×·ü
+		STATE_JUMP,
+		STATE_RUN,
+		NUM_STATES,
+	};
+
+
 };
