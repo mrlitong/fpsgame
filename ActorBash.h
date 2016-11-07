89eea0bf629eb818a5ca5bd5163721fae97ba305
mrlitong <litongtongxue@gmail.com>
2016-11-07 11:05:25 +0800

    本类所使用的一些私有变量的定义
    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- ActorBash.h
+++ ActorBash.h
@@ -124,7 +124,55 @@ public:
 
     virtual void  RenderVisualizer();
 private:
-	
+	    // update bounds	//更新边界
+    void Update_Bounds();
+        
+    // player transformation
+    MathLib::Mat4 Get_Body_Transform() const;
+   
+    // update states
+    int Update_State(int condition,int state,int begin,int end,float ifps);
+    void Update_States(int enabled,float ifps);
+        
+    int m_nFrame;                   // frame number
+    
+    MathLib::vec3   m_vUp;          // up  三维坐标中的向量
+    MathLib::vec3   m_vVelocity;    // velocity vector	当前速度向量
+
+    CObjectDummy *m_pObject;        // dummy object
+    CBodyDummy *m_pDummy;           // dummy body
+        
+    int m_nCollision;               // collision flag
+    CShapeCapsule *m_pShape;        // collision shape
+ 
+    float m_fFriction;              // fraction when frozen
+    float m_fMinVelocity;           // minimum velocity
+    float m_fMaxVelocity;           // maximum velocity
+    float m_fAcceleration;          // acceleration
+    float m_fDamping;               // damping
+    float m_fJumping;               // jumping
+        
+    int m_nFlush;                   // flush flag
+    MathLib::Vec3 m_vPosition;      // position
+    MathLib::vec3 m_vDirection;     // direction
+    float m_fPhiAngle;              // phi angle
+    float m_fThetaAngle;            // theta angle
+        
+    int m_pStates[NUM_STATES];      // state vector
+    float m_pTimes[NUM_STATES];     // time vector
+        
+    int m_nGround;                  // ground flag
+    int m_nCeiling;                 // ceiling flag
+        
+    CBoundBox m_BoundBox;           // bounding box
+    CBoundSphere m_BoundSphere;     // bounding sphere
+
+    CWorldBoundBox m_WorldBoundBox;           // bounding box
+    CWorldBoundSphere m_WorldBoundSphere;     // bounding sphere
+
+    CVector<CShape::Contact> m_vecContacts;
+    int m_nEnable;
 
 
 };
+#endif  __PLAYER_ACTOR_H__ 
