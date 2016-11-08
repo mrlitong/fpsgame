9c00ecf50c630948f9806f62fe90a25435c2c95a
mrlitong <litongtongxue@gmail.com>
2016-11-08 14:19:26 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- CameraBash.h
+++ CameraBash.h
@@ -4,6 +4,37 @@
 
 class CPlayer;
 
+/*
+*包括摄像头角度，视觉半径，最大最小角度，转向，视线方向，等。
+*/
+class CCameraBase : public CPlayer
+{
+public:
 
+	CCameraBase();
+	virtual ~CCameraBase();
+
+	void SetEnabled(int nEnable);
+	int IsEnabled() const;
+
+	void SetHMDEnabled(int nEnable);
+	int IsHMDEnabled() const;
+
+	void SetRadius(float r);
+	float GetRadius() const;
+
+	// minimum theta angle
+	void SetMinThetaAngle(float angle);
+	float GetMinThetaAngle() const;
+
+	// maximum theta angle
+	void SetMaxThetaAngle(float angle);
+	float GetMaxThetaAngle() const;
+
+	// turning
+	void SetTurning(float turning);
+	float GetTurning() const;
+
+}
 
 #endif
