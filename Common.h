de9724d0ed5aaeef9c0d97edc92af18fcf4f7b1a
mrlitong <litongtongxue@gmail.com>
2016-11-16 13:55:50 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- Common.h
+++ Common.h
@@ -1,7 +1,12 @@
+#pragma once
+#include "MathLib.h"
+using namespace MathLib;
+class CBRObject;
+
 class CCommon
 {
 public:
 	CCommon(void);
 	~CCommon(void);
-
+	static int	 GetIntersectionObject(CBRObject** vRetObject, const vec3& p0, const vec3& p1, int nMask = 2 | 4);
 };
