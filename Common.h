7a2473c05fa3b540bc0ae230eb783333178dbad0
mrlitong <litongtongxue@gmail.com>
2016-11-16 18:27:21 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- Common.h
+++ Common.h
@@ -9,6 +9,7 @@ public:
 	CCommon(void);
 	~CCommon(void);
 	static int	 GetIntersectionObject(CBRObject** vRetObject, const vec3& p0, const vec3& p1, int nMask = 2 | 4);
-
-
+	static float  CalcAxisScale(float fov,vec4 objectPosW,float sizeInPixels,float viewHeight);
+	static vec3   GetTransformDirection(const mat4& matTransform,const vec3& vUP = vec3(0.0f,0.0f,1.0f));
+	static int	 GetIntersectionPosition(vec3& vRetPoint,const vec3& p0,const vec3& p1,int nMask = 2|4);
 };
