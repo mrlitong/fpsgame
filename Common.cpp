8ce53030ad7f448ea99a309be77402d8849bcd07
mrlitong <litongtongxue@gmail.com>
2016-11-17 13:23:19 +0800

    vector set

--- Common.cpp
+++ Common.cpp
@@ -31,10 +31,20 @@ int CCommon::GetIntersectionPosition(vec3& vRetPoint, const vec3& p0, const vec3
 
 int CCommon::GetIntersectionObject(CBRObject** vRetObject, const vec3& p0, const vec3& p1, int nMask /*= 2|4*/)
 {
+	vec3 p, n;
+	int s;
+	*vRetObject = g_Engine.pWorld->GetIntersection(p0, p1, nMask, p, n, s);
 
+	return s >= 0;
 }
 
 float CCommon::CalcAxisScale(const mat4& modelview, float fov, vec4 objectPosW, float sizeInPixels, float viewHeight)
 {
+	float worldHeight;
+	// World height on origin's z value
+	vec4 objPosV;
+	objPosV = modelview * objectPosW;
+	worldHeight = 2.0f * CMathCore::Abs(objPosV.z) * (float)CMathCore::Tan(fov / 2.0f * DEG2RAD);
 
+	return sizeInPixels * (worldHeight / viewHeight);
 }
