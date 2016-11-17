c40f88e1db5df3656f12f5cfa490f41924798d93
mrlitong <litongtongxue@gmail.com>
2016-11-17 10:14:16 +0800

    change the nMask

--- Common.h
+++ Common.h
@@ -11,5 +11,6 @@ public:
 	static int	 GetIntersectionObject(CBRObject** vRetObject, const vec3& p0, const vec3& p1, int nMask = 2 | 4);
 	static float  CalcAxisScale(float fov,vec4 objectPosW,float sizeInPixels,float viewHeight);
 	static vec3   GetTransformDirection(const mat4& matTransform,const vec3& vUP = vec3(0.0f,0.0f,1.0f));
-	static int	 GetIntersectionPosition(const mat4& modelview,vec3& vRetPoint,const vec3& p0,const vec3& p1,int nMask = 2|4);
+	static int	 GetIntersectionPosition(const mat4& modelview,vec3& vRetPoint,const vec3& p0,const vec3& p1,int nMask = 2|3);
+
 };
