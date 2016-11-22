1195072d21781193e6745009ce9d1ab0cd7ca49d
mrlitong <litongtongxue@gmail.com>
2016-11-22 09:55:54 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- FPSRole.cpp
+++ FPSRole.cpp
@@ -1,10 +1,15 @@
 #include "FPSRole.h"
+#include "Engine.h"
 
 CFPSRole::CFPSRole(void)
 {
-
+	m_nFire = 0;
+	m_fAniCoolingTime = 0;
+	m_fAniNowCoolingTime = 0;
+	m_fSudCoolingTime = 0;	
+	m_fEmitCoolingTime = 0;
 }
 CFPSRole::~CFPSRole(void)
 {
-
+	g_Engine.pGame->RemoveNode(m_pMuzzleEffect);
 }
