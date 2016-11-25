c67385d59bf288345c66f6a6ca6fa6d9f5116b66
mrlitong <litongtongxue@gmail.com>
2016-11-25 10:17:49 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- CameraBash.cpp
+++ CameraBash.cpp
@@ -12,7 +12,22 @@ using namespace MathLib;
 
 CCameraBase::CCameraBase(): CPlayer(PLAYER_SPECTATOR) 
 {
+	m_nFlush = 0;
+    m_vPosition = Vec3_zero;
+    m_fPhiAngle = 0.0f;
+    m_fThetaAngle = 0.0f;
+    m_pLastPlayer = NULL;
+	m_nEnabelMouse = 1;
+    SetRadius(1.0f);
 
+	SetMinThetaAngle(-90.0f);
+    SetMaxThetaAngle(90.0f);
+    SetTurning(90.0f);
+	
+    SetViewDirection(vec3(0.0f,1.0f,0.0f));
+
+    SetEnabled(0);
+    SetHMDEnabled(1);
 }
 
 CCameraBase::~CCameraBase()
