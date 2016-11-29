93a8e3c81a9441ea6250910916411c8588c50c8e
mrlitong <litongtongxue@gmail.com>
2016-11-29 17:30:46 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- GameProcess.h
+++ GameProcess.h
@@ -9,4 +9,17 @@ class CGameProcess : public CMessageBase
 public:
 	CGameProcess(void);
 	virtual ~CGameProcess(void);
+	int        Init();
+	int        ShutDown();
+	int        Update();
+	int        Render();
+
+	CRoleBase*  GetRole(int nID);
+	CRoleBase*  GetRoleFormIndex(int nIndex);
+
+public:
+	static int  KeyPress(unsigned int nKey);
+	static int  KeyRelease(unsigned int nKey);
+
+
 };
