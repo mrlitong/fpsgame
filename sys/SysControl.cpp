224eb76619cfd4ec0082d9496c06ac44b24495fc
mrlitong <litongtongxue@gmail.com>
2016-12-08 16:50:26 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- sys/SysControl.cpp
+++ sys/SysControl.cpp
@@ -1,1 +1,14 @@
 #include "SysControl.h"
+#include "ControlsApp.h"
+
+//解决跨平台字符集兼容问题
+#ifdef _WIN32
+#pragma execution_character_set("utf-8")
+#endif
+
+using namespace MathLib;
+
+class CSysControlLocal : public CSysControl
+{
+
+}
