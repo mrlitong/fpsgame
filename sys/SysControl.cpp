919f7eaf85e35ce13aad639ca2d42bef82c15f56
mrlitong <litongtongxue@gmail.com>
2016-12-09 22:28:24 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- sys/SysControl.cpp
+++ sys/SysControl.cpp
@@ -1,5 +1,6 @@
 #include "SysControl.h"
 #include "ControlsApp.h"
+#include "ControlsXPad360.h"
 
 //解决跨平台字符集兼容问题
 #ifdef _WIN32
@@ -11,6 +12,14 @@ using namespace MathLib;
 class CSysControlLocal : public CSysControl
 {
 public:
-	    CSysControlLocal();
+	CSysControlLocal();
     virtual ~CSysControlLocal();
+
+	virtual void Init();	//角色要初始化，着色器要初始化，
+	virtual void Update(float ifps);
+	virtual void Shutdown();
+
+
+
+
 }
