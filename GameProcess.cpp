2df40c0b03dee3f428064872424c48b6db2bd565
mrlitong <litongtongxue@gmail.com>
2016-12-28 23:41:19 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- GameProcess.cpp
+++ GameProcess.cpp
@@ -1,5 +1,7 @@
 #include "GameProcess.h"
-
+#include "Object.h"
+#include "Engine.h"
+#include "World.h"
 using namespace MathLib;
 
 CGameProcess::CGameProcess(void)
@@ -7,3 +9,7 @@ CGameProcess::CGameProcess(void)
 
 }
 
+CGameProcess::~CGameProcess(void)
+{
+
+}
