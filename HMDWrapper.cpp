ca289a0048a8d9db2e0fc3cd8886ee553bd21c49
mrlitong <litongtongxue@gmail.com>
2017-03-04 10:19:50 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- HMDWrapper.cpp
+++ HMDWrapper.cpp
@@ -2,3 +2,20 @@
 #include "interface\DeviceMgrInterface.h"
 
 using namespace MathLib;
+
+class CLocalHMDWrapper : public CHMDWrapper
+{
+public:
+
+	CLocalHMDWrapper();
+	virtual ~CLocalHMDWrapper();
+	virtual void                Init();
+	virtual void                Update();
+	virtual void                Shutdown();
+	virtual void                SetUseHMD(int u);
+	virtual int                 GetUseHMD() const;
+	virtual const MathLib::vec3 &GetLocalDirection() const;
+	virtual const MathLib::vec3 &GetLocalUp() const;
+
+
+}
