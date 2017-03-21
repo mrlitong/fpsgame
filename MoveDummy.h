a73111661eebd5e00237421734037672a6a927da
litong <litongtongxue@gmail.com>
2017-03-21 09:05:18 +0800

    SetEnabled GetEnabled

--- MoveDummy.h
+++ MoveDummy.h
@@ -38,6 +38,8 @@ public:
 	void                    SetUp(const MathLib::vec3& u);
 	const MathLib::vec3&    GetUp() const;
 
+	void                    SetEnabled(int e);
+	int                     GetEnabled() const;
 
 
 
