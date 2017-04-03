8437e1e9678530477c653b276a0a348de9881428
mrlitong <litongtongxue@gmail.com>
2017-04-03 15:22:27 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- StartControl.h
+++ StartControl.h
@@ -11,5 +11,13 @@ class CStarControl : public CSingleton < CStarControl >
 public:
 	CStarControl(void);
 	~CStarControl(void);
-
+public:
+	int		Init();
+	void		Update(const vec3& vPos, const vec3& vDir);
+	void		Click();
+	int		Load(const char* strNormal, const char* strClick);
+	int		isEnable();
+	void		SetEnable(int nEnable);
+	void		SetViewDistance(float fViewDistance) { m_fViewDistance = fViewDistance; }
+	void		SetColor(const vec4& vColor);
 }
