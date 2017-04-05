03bcbe05666916ba60bf7ae5dcae3660a83cb082
mrlitong <litongtongxue@gmail.com>
2017-04-05 23:49:16 +0800

    CStarControl

--- StartControl.cpp
+++ StartControl.cpp
@@ -6,3 +6,13 @@
 #include "Player.h"
 #include "Common.h"
 #include "App.h"
+
+CStarControl::CStarControl(void)
+{
+	m_pStarNormal = NULL;
+	m_pStarClick = NULL;
+	m_fViewDistance = 0.5f;
+	m_nClickState = 0;
+	m_fClickScale = 1.0f;
+	Init();
+}
