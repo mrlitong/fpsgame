b359d5fe1cf087b3401e03c4b06851a04174aae1
mrlitong <litongtongxue@gmail.com>
2017-04-08 17:20:27 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- RoleBase.h
+++ RoleBase.h
@@ -7,7 +7,6 @@ class CAction;
 class CInterfaceBase;
 class CNavigationFinder;
 
-
 struct _ActionCallback_KeyFrame;
 struct _ActionCallback_Complete;
 using namespace MathLib;
@@ -18,6 +17,7 @@ class CRoleBase
 public:
 	CRoleBase(void);
 	virtual ~CRoleBase(void);
+
 	virtual int	Init(int nRoleID, const char* strCharFile);
 	virtual void	Update(float ifps);
 
@@ -26,14 +26,18 @@ public:
 
 	CAction*	PlayAction(const char* szName, int nLoop = 0, float fCorrectingTime = 1.0f);
 	CAction*	OrceAction(const char* szName, int nLoop = 0, float fCorrectingTime = 1.0f);
+
 	void		SetPosition(const vec3& vPosition, int nOrce = 0);
 	void		SetDirection(const vec3& vDirection, int nOrce = 0);
 	void		SetDirectionTo(const vec3& vDirection, int nOrce = 0);
+
 	virtual int	MoveTo(const vec3& vPosition);
 	virtual int	MoveToPath(const vec3& vPosition);
 	virtual void	StopMove();
 	virtual void	StopMove(const vec3& vPosition);
+
 	int		IsMoveing() { return m_nMoveing; }
+
 	vec3		GetDirection();
 	vec3		GetPosition();
 
