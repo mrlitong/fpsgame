c8e714d0e42b6f23ef0267ba76484c885fd96c42
mrlitong <litongtongxue@gmail.com>
2017-04-09 23:19:02 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- Star2DControl.h
+++ Star2DControl.h
@@ -14,6 +14,15 @@ public:
 	CStar2DControl(void);
 	~CStar2DControl(void);
 
+public:
+
+	int         Init();
+	void        Update();
+	void        Click();
+	int         Load(const char* strNormal, const char* strClick);
+	void        SetEnable(int nEnable);
+	int         IsEnable();
+
 
 
 }
