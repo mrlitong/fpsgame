4383155945dda922f754232d2537b7dc1c6588b6
litong <litongtongxue@gmail.com>
2017-04-11 12:08:15 +0800

    Signed-off-by: litong <litongtongxue@gmail.com>

--- Star2DControl.cpp
+++ Star2DControl.cpp
@@ -3,3 +3,16 @@
 
 using namespace MathLib;
 
+CStar2DControl::CStar2DControl(void)
+{
+	m_pNormalTex = NULL;
+	m_pClickTex = NULL;
+	m_nEnabled = 1;
+	m_nClickState = 0;
+	m_fClickScale = 1.0f;
+	m_vNormalColor = vec4_one;
+	m_vClickColor = vec4_one;
+	m_fNormalTexScale = 0.1f;
+	m_fClickTexScale = 0.1f;
+	Init();
+}
