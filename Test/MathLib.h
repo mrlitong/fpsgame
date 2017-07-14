3071baa6bcc04a568e522e850dbcbd7072741f8c
mrlitong <litongtongxue@gmail.com>
2017-07-14 10:10:44 +0800

    add some common math function

--- Test/MathLib.h
+++ Test/MathLib.h
@@ -6,4 +6,33 @@
 
 //Make some change to check if new branch is created.
 
+#include <iostream>
+
+using namespace std;
+
+//------------------------------------------------------------
+double integral(double(*f)(double), double a = 0, double b = 1, double tol = 1e-5, long n = 50);
+//------------------------------------------------------------
+double limit(double(*f)(double), double a = 0, double b = 1, double tol = 1e-5, long n = 1);
+//------------------------------------------------------------
+double diff(double(*f)(double), double a = 0, double b = 1, double tol = 1e-5, long n = 1);
+//------------------------------------------------------------
+#define PLOT_WIDTH 60
+double plot(double x[], int n, char c = '*');
+//------------------------------------------------------------
+double fplot(double(*f)(double), double a = 0, double b = 1, int n = 25, char c = '*');
+//------------------------------------------------------------
+int subset(double *a, int i, int j);
+
+void subsort(double *a, int i, int j);
+//------------------------------------------------------------
+void shellsort(double *a, int n);
+//------------------------------------------------------------
+void cmpsort(double *a, int n);
+//------------------------------------------------------------
+void rollsort(double *a, int n);
+//------------------------------------------------------------
+
+
+
 #endif
