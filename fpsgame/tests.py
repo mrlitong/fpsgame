9af25d1ee342f6d8e9205912bb66a99595e767f8
mrlitong <litongtongxue@gmail.com>
2017-07-19 20:53:55 +0800

    Add python dll.

--- fpsgame/tests.py
+++ fpsgame/tests.py
@@ -10,3 +10,7 @@ dll_filename = {
 	'posix': './libCollada_dbg.so',
 	'nt': 'Collada_dbg.dll',
 }[os.name]
+
+# The DLL may need other DLLs which are in its directory, so set the path to that
+# (Don't care about clobbering the old PATH - it doesn't have anything important)
+os.environ['PATH'] = '%s/system/' % binaries
