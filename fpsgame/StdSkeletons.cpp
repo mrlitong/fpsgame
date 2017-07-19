025c6ed8460335698502d06c2ec1c36e5abc95ae
mrlitong <litongtongxue@gmail.com>
2017-07-19 20:57:58 +0800

    Skeletons struct init.

---
+++ fpsgame/StdSkeletons.cpp
@@ -0,0 +1,19 @@
+#include "precompiled.h"
+
+namespace
+{
+	struct SkeletonMap
+	{
+		SkeletonMap()
+		{
+
+		}
+		~SkeletonMap()
+		{
+
+		}
+	};
+	SkeletonMap g_StandardSkeletons;
+	SkeletonMap g_MappedSkeletons;
+
+}
