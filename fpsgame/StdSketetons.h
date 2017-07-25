79ff5ea2e1b0f4205ba80280968bf63e6152081c
mrlitong <litongtongxue@gmail.com>
2017-07-25 10:28:48 +0800

    Get boneCount,

--- fpsgame/StdSketetons.h
+++ fpsgame/StdSketetons.h
@@ -2,6 +2,9 @@
 #ifndef INCLUDED_STDSKELETONS
 #define INCLUDED_STDSKELETONS
 
+#include <string>
+#include <memory>
+
 struct Skeleton_impl;
 
 class Sketeton
@@ -10,4 +13,15 @@ public:
 	/** Default constructor - don't use; use FindSkeleton instead. */
 	Skeleton();
 	~Skeleton();
+
+	/**
+	* Returns the number of bones in the standard-skeleton which this current
+	* skeleton is mapped onto.
+	*/
+	int GetBoneCount() const;
+
+
+
 };
+
+#endif
