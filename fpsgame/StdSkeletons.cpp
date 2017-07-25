6c773515729185215028f87c99b94fb16cba0ca3
mrlitong <litongtongxue@gmail.com>
2017-07-25 10:26:34 +0800

    add skeletons

--- fpsgame/StdSkeletons.cpp
+++ fpsgame/StdSkeletons.cpp
@@ -1,8 +1,20 @@
 #include "precompiled.h"
 
+
+#include "libxml/parser.h"
+#include "libxml/xmlerror.h"
+
+#include "StdSkeletons.h"
+
+#include "CommonConvert.h"
+
+#include "FUtils/FUXmlParser.h"
+
+#include <map>
+
 namespace
 {
-	struct SkeletonMap
+	struct SkeletonMap : public std::map<std::string, const Skeleton*>
 	{
 		SkeletonMap()
 		{
@@ -10,10 +22,22 @@ namespace
 		}
 		~SkeletonMap()
 		{
-
+			for (iterator it = begin(); it != end(); ++it)
+				delete it->second;
 		}
 	};
 	SkeletonMap g_StandardSkeletons;
 	SkeletonMap g_MappedSkeletons;
 
+	struct Bone
+	{
+		std::string parent;
+		std::string name;
+		int targetId;
+		int realTargetId;
+	};
 }
+
+
+Skeleton::Skeleton() : m(new Skeleton_impl) { }
+Skeleton::~Skeleton() { }
---
+++ fpsgame/StdSketetons.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef INCLUDED_STDSKELETONS
+#define INCLUDED_STDSKELETONS
+
+struct Skeleton_impl;
+
+class Sketeton
+{
+public:
+	/** Default constructor - don't use; use FindSkeleton instead. */
+	Skeleton();
+	~Skeleton();
+};
