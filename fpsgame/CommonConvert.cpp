d0a9c8dbecd402f42c8ba2f37900dd1451ca0430
mrlitong <litongtongxue@gmail.com>
2017-07-31 09:21:26 +0800

    Signed-off-by: mrlitong <litongtongxue@gmail.com>

--- fpsgame/CommonConvert.cpp
+++ fpsgame/CommonConvert.cpp
@@ -16,6 +16,10 @@ void require_(int line, bool value, const char* type, const char* message)
 	throw ColladaException(std::string(type) + " (line " + linestr + "): " + message);
 }
 
+const FMVector3 FMVector3_XAxis(1.0f, 0.0f, 0.0f);
+static float identity[] = { 1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1 };
+FMMatrix44 FMMatrix44_Identity(identity);
+
 /** Error handler for libxml2 */
 void errorHandler(void* ctx, const char* msg, ...)
 {
@@ -27,4 +31,21 @@ void errorHandler(void* ctx, const char* msg, ...)
 	va_end(ap);
 
 	*((std::string*)ctx) += buffer;
+}
+
+FColladaErrorHandler::FColladaErrorHandler(std::string& xmlErrors_)
+	: xmlErrors(xmlErrors_)
+{
+	// Grab all the error output from libxml2, for useful error reporting
+	xmlSetGenericErrorFunc(&xmlErrors, &errorHandler);
+
+	FUError::AddErrorCallback(FUError::DEBUG_LEVEL, this, &FColladaErrorHandler::OnError);
+	FUError::AddErrorCallback(FUError::WARNING_LEVEL, this, &FColladaErrorHandler::OnError);
+	FUError::AddErrorCallback(FUError::ERROR_LEVEL, this, &FColladaErrorHandler::OnError);
+}
+
+
+CommonConvert::~CommonConvert()
+{
+	
 }
