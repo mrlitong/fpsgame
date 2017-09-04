efc4716da7432348e7d6d501d4dae0591f4735b7
mrlitong <litongtongxue@gmail.com>
2017-09-04 21:34:21 +0800

    ADD：private texture loader helper functions

---
+++ fpsgame/gui/tex/tex_internal.h
@@ -0,0 +1,61 @@
+/* Copyright (c) 2010 Wildfire Games
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * private texture loader helper functions
+ */
+
+#ifndef INCLUDED_TEX_INTERNAL
+#define INCLUDED_TEX_INTERNAL
+
+#include "lib/pointer_typedefs.h"
+#include "lib/allocators/dynarray.h"
+#include "lib/file/io/io.h"	// io::Allocate
+
+/**
+ * check if the given texture format is acceptable: 8bpp grey,
+ * 24bpp color or 32bpp color+alpha (BGR / upside down are permitted).
+ * basically, this is the "plain" format understood by all codecs and
+ * tex_codec_plain_transform.
+ * @param bpp bits per pixel
+ * @param flags TexFlags
+ * @return Status
+ **/
+extern Status tex_validate_plain_format(size_t bpp, size_t flags);
+
+
+/**
+ * indicate if the two vertical orientations match.
+ *
+ * used by tex_codec.
+ * 
+ * @param src_flags TexFlags, used to extract the orientation.
+ * we ask for this instead of src_orientation so callers don't have to
+ * mask off TEX_ORIENTATION.
+ * @param dst_orientation orientation to compare against.
+ * can be one of TEX_BOTTOM_UP, TEX_TOP_DOWN, or 0 for the
+ * "global orientation".
+ * @return bool
+ **/
+extern bool tex_orientations_match(size_t src_flags, size_t dst_orientation);
+
+#endif	// #ifndef INCLUDED_TEX_INTERNAL
