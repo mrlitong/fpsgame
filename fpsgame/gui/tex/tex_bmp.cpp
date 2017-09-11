f1cbbd1eadda572cf69738534325c909e1ca4164
mrlitong <litongtongxue@gmail.com>
2017-09-11 11:01:35 +0800

    MOD：support routines and interface for texture codecs.

---
+++ fpsgame/gui/tex/tex_bmp.cpp
@@ -0,0 +1,157 @@
+/* Copyright (c) 2010 Wildfire Games
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * Windows BMP codec
+ */
+
+#include "precompiled.h"
+
+#include "lib/byte_order.h"
+#include "tex_codec.h"
+
+#pragma pack(push, 1)
+
+struct BmpHeader
+{ 
+	// BITMAPFILEHEADER
+	u16 bfType;			// "BM"
+	u32 bfSize;			// of file
+	u16 bfReserved1;
+	u16 bfReserved2;
+	u32 bfOffBits;		// offset to image data
+
+	// BITMAPINFOHEADER
+	u32 biSize;
+	i32 biWidth;
+	i32 biHeight;
+	u16 biPlanes;
+	u16 biBitCount;
+	u32 biCompression;
+	u32 biSizeImage;
+	// the following are unused and zeroed when writing:
+	i32 biXPelsPerMeter;
+	i32 biYPelsPerMeter;
+	u32 biClrUsed;
+	u32 biClrImportant;
+};
+
+#pragma pack(pop)
+
+#define BI_RGB 0		// biCompression
+
+
+Status TexCodecBmp::transform(Tex* UNUSED(t), size_t UNUSED(transforms)) const
+{
+	return INFO::TEX_CODEC_CANNOT_HANDLE;
+}
+
+
+bool TexCodecBmp::is_hdr(const u8* file) const
+{
+	// check header signature (bfType == "BM"?).
+	// we compare single bytes to be endian-safe.
+	return (file[0] == 'B' && file[1] == 'M');
+}
+
+
+bool TexCodecBmp::is_ext(const OsPath& extension) const
+{
+	return extension == L".bmp";
+}
+
+
+size_t TexCodecBmp::hdr_size(const u8* file) const
+{
+	const size_t hdr_size = sizeof(BmpHeader);
+	if(file)
+	{
+		BmpHeader* hdr = (BmpHeader*)file;
+		const u32 ofs = read_le32(&hdr->bfOffBits);
+		ENSURE(ofs >= hdr_size && "bmp_hdr_size invalid");
+		return ofs;
+	}
+	return hdr_size;
+}
+
+
+// requirements: uncompressed, direct color, bottom up
+Status TexCodecBmp::decode(rpU8 data, size_t UNUSED(size), Tex* RESTRICT t) const
+{
+	const BmpHeader* hdr = (const BmpHeader*)data;
+	const long w       = (long)read_le32(&hdr->biWidth);
+	const long h_      = (long)read_le32(&hdr->biHeight);
+	const u16 bpp      = read_le16(&hdr->biBitCount);
+	const u32 compress = read_le32(&hdr->biCompression);
+
+	const long h = abs(h_);
+
+	size_t flags = 0;
+	flags |= (h_ < 0)? TEX_TOP_DOWN : TEX_BOTTOM_UP;
+	if(bpp > 16)
+		flags |= TEX_BGR;
+	if(bpp == 32)
+		flags |= TEX_ALPHA;
+
+	// sanity checks
+	if(compress != BI_RGB)
+		WARN_RETURN(ERR::TEX_COMPRESSED);
+
+	t->m_Width  = w;
+	t->m_Height = h;
+	t->m_Bpp    = bpp;
+	t->m_Flags  = flags;
+	return INFO::OK;
+}
+
+
+Status TexCodecBmp::encode(Tex* RESTRICT t, DynArray* RESTRICT da) const
+{
+	const size_t hdr_size = sizeof(BmpHeader);	// needed for BITMAPFILEHEADER
+	const size_t img_size = t->img_size();
+	const size_t file_size = hdr_size + img_size;
+	const i32 h = (t->m_Flags & TEX_TOP_DOWN)? -(i32)t->m_Height : (i32)t->m_Height;
+
+	size_t transforms = t->m_Flags;
+	transforms &= ~TEX_ORIENTATION;	// no flip needed - we can set top-down bit.
+	transforms ^= TEX_BGR;			// BMP is native BGR.
+
+	const BmpHeader hdr =
+	{
+		// BITMAPFILEHEADER
+		0x4D42,				// bfType = 'B','M'
+		(u32)file_size,		// bfSize
+		0, 0,				// bfReserved1,2
+		hdr_size,			// bfOffBits
+
+		// BITMAPINFOHEADER
+		40,					// biSize = sizeof(BITMAPINFOHEADER)
+		(i32)t->m_Width,
+		h,
+		1,					// biPlanes
+		(u16)t->m_Bpp,
+		BI_RGB,				// biCompression
+		(u32)img_size,		// biSizeImage
+		0, 0, 0, 0			// unused (bi?PelsPerMeter, biClr*)
+	};
+	return tex_codec_write(t, transforms, &hdr, hdr_size, da);
+}
---
+++ fpsgame/gui/tex/tex_codec.h
@@ -0,0 +1,244 @@
+/* Copyright (c) 2010 Wildfire Games
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * support routines and interface for texture codecs.
+ */
+
+#ifndef INCLUDED_TEX_CODEC
+#define INCLUDED_TEX_CODEC
+
+#include "tex.h"
+#include "tex_internal.h"	// for codec's convenience
+
+/**
+ * virtual method table for TexCodecs.
+ * rationale: this works in C and also allows storing name and next in vtbl.
+ * 'template method'-style interface to increase code reuse and
+ * simplify writing new codecs.
+ **/
+class ITexCodec
+{
+public:
+	/**
+	 * decode the file into a Tex structure.
+	 *
+	 * @param data input data array (non-const, because the texture
+	 * may have to be flipped in-place - see "texture orientation").
+	 * @param size [bytes] of data, always >= 4
+	 *   (this is usually enough to compare the header's "magic" field,
+	 *    and no legitimate file will be smaller)
+	 * @param t output texture object
+	 * @return Status
+	 **/
+	virtual Status decode(u8* data, size_t size, Tex* RESTRICT t) const = 0;
+
+	/**
+	 * encode the texture data into the codec's file format (in memory).
+	 *
+	 * @param t input texture object. note: non-const because encoding may
+	 * require a Tex::transform.
+	 * @param da output data array, allocated by codec.
+	 * rationale: some codecs cannot calculate the output size beforehand
+	 * (e.g. PNG output via libpng), so the output memory cannot be allocated
+	 * by the caller.
+	 * @return Status
+	 **/
+	virtual Status encode(Tex* RESTRICT t, DynArray* RESTRICT da) const = 0;
+
+	/**
+	 * transform the texture's pixel format.
+	 *
+	 * @param t texture object
+	 * @param transforms: OR-ed combination of TEX_* flags that are to
+	 * be changed. note: the codec needs only handle situations specific
+	 * to its format; generic pixel format transforms are handled by
+	 * the caller.
+	 **/
+	virtual Status transform(Tex* t, size_t transforms) const = 0;
+
+	/**
+	 * indicate if the data appears to be an instance of this codec's header,
+	 * i.e. can this codec decode it?
+	 *
+	 * @param file input data; only guaranteed to be 4 bytes!
+	 * (this should be enough to examine the header's 'magic' field)
+	 * @return bool
+	 **/
+	virtual bool is_hdr(const u8* file) const = 0;
+
+	/**
+	 * is the extension that of a file format supported by this codec?
+	 *
+	 * rationale: cannot just return the extension string and have
+	 * caller compare it (-> smaller code) because a codec's file format
+	 * may have several valid extensions (e.g. jpg and jpeg).
+	 *
+	 * @param extension (including '.')
+	 * @return bool
+	 **/
+	virtual bool is_ext(const OsPath& extension) const = 0;
+
+	/**
+	 * return size of the file header supported by this codec.
+	 *
+	 * @param file the specific header to return length of (taking its
+	 * variable-length fields into account). if NULL, return minimum
+	 * guaranteed header size, i.e. the header without any
+	 * variable-length fields.
+	 * @return size [bytes]
+	 **/
+	virtual size_t hdr_size(const u8* file) const = 0;
+
+	/**
+	 * name of codec for debug purposes. typically set via TEX_CODEC_REGISTER.
+	 **/
+	virtual const wchar_t* get_name() const = 0;
+
+	virtual ~ITexCodec() {}
+};
+
+class TexCodecPng:ITexCodec {
+public:
+	virtual Status decode(u8* data, size_t size, Tex* RESTRICT t) const;
+	virtual Status encode(Tex* RESTRICT t, DynArray* RESTRICT da) const;
+	virtual Status transform(Tex* t, size_t transforms) const;
+	virtual bool is_hdr(const u8* file) const;
+	virtual bool is_ext(const OsPath& extension) const;
+	virtual size_t hdr_size(const u8* file) const;
+	virtual const wchar_t* get_name() const {
+		static const wchar_t *name = L"png";
+		return name;
+	};
+};
+
+class TexCodecDds:ITexCodec {
+public:
+	virtual Status decode(u8* data, size_t size, Tex* RESTRICT t) const;
+	virtual Status encode(Tex* RESTRICT t, DynArray* RESTRICT da) const;
+	virtual Status transform(Tex* t, size_t transforms) const;
+	virtual bool is_hdr(const u8* file) const;
+	virtual bool is_ext(const OsPath& extension) const;
+	virtual size_t hdr_size(const u8* file) const;
+	virtual const wchar_t* get_name() const {
+		static const wchar_t *name = L"dds";
+		return name;
+	};
+};
+
+class TexCodecTga:ITexCodec {
+public:
+	virtual Status decode(u8* data, size_t size, Tex* RESTRICT t) const;
+	virtual Status encode(Tex* RESTRICT t, DynArray* RESTRICT da) const;
+	virtual Status transform(Tex* t, size_t transforms) const;
+	virtual bool is_hdr(const u8* file) const;
+	virtual bool is_ext(const OsPath& extension) const;
+	virtual size_t hdr_size(const u8* file) const;
+	virtual const wchar_t* get_name() const {
+		static const wchar_t *name = L"tga";
+		return name;
+	};
+};
+
+class TexCodecBmp:ITexCodec {
+public:
+	virtual Status decode(u8* data, size_t size, Tex* RESTRICT t) const;
+	virtual Status encode(Tex* RESTRICT t, DynArray* RESTRICT da) const;
+	virtual Status transform(Tex* t, size_t transforms) const;
+	virtual bool is_hdr(const u8* file) const;
+	virtual bool is_ext(const OsPath& extension) const;
+	virtual size_t hdr_size(const u8* file) const;
+	virtual const wchar_t* get_name() const {
+		static const wchar_t *name = L"bmp";
+		return name;
+	};
+};
+
+/**
+ * Find codec that recognizes the desired output file extension.
+ *
+ * @param extension
+ * @param c (out) vtbl of responsible codec
+ * @return Status; ERR::RES_UNKNOWN_FORMAT (without warning, because this is
+ * called by tex_is_known_extension) if no codec indicates they can
+ * handle the given extension.
+ **/
+extern Status tex_codec_for_filename(const OsPath& extension, const ITexCodec** c);
+
+/**
+ * find codec that recognizes the header's magic field.
+ *
+ * @param data typically contents of file, but need only include the
+ * (first 4 bytes of) header.
+ * @param data_size [bytes]
+ * @param c (out) vtbl of responsible codec
+ * @return Status; ERR::RES_UNKNOWN_FORMAT if no codec indicates they can
+ * handle the given format (header).
+ **/
+extern Status tex_codec_for_header(const u8* data, size_t data_size, const ITexCodec** c);
+
+/**
+ * transform the texture's pixel format.
+ * tries each codec's transform method once, or until one indicates success.
+ *
+ * @param t texture object
+ * @param transforms: OR-ed combination of TEX_* flags that are to
+ * be changed.
+ * @return Status
+ **/
+extern Status tex_codec_transform(Tex* t, size_t transforms);
+
+/**
+ * allocate an array of row pointers that point into the given texture data.
+ * for texture decoders that support output via row pointers (e.g. PNG),
+ * this allows flipping the image vertically (useful when matching bottom-up
+ * textures to a global orientation) directly, which is much more
+ * efficient than transforming later via copying all pixels.
+ *
+ * @param data the texture data into which row pointers will point.
+ * note: we don't allocate it here because this function is
+ * needed for encoding, too (where data is already present).
+ * @param h height [pixels] of texture.
+ * @param pitch size [bytes] of one texture row, i.e. width*bytes_per_pixel.
+ * @param src_flags TexFlags of source texture. used to extract its
+ * orientation.
+ * @param dst_orientation desired orientation of the output data.
+ * can be one of TEX_BOTTOM_UP, TEX_TOP_DOWN, or 0 for the
+ * "global orientation".
+ * depending on src and dst, the row array is flipped if necessary.
+ **/
+typedef const u8* RowPtr;
+extern std::vector<RowPtr> tex_codec_alloc_rows(const u8* data, size_t h, size_t pitch, size_t src_flags, size_t dst_orientation);
+
+/**
+ * apply transforms and then copy header and image into output buffer.
+ *
+ * @param t input texture object
+ * @param transforms transformations to be applied to pixel format
+ * @param hdr header data
+ * @param hdr_size [bytes]
+ * @param da output data array (will be expanded as necessary)
+ * @return Status
+ **/
+extern Status tex_codec_write(Tex* t, size_t transforms, const void* hdr, size_t hdr_size, DynArray* da);
+
+#endif	 // #ifndef INCLUDED_TEX_CODEC
