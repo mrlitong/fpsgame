89263911dced0e99357cb1a3fc4f4d6fea73ac37
mrlitong <litongtongxue@gmail.com>
2017-09-27 22:17:05 +0800

    ADD:Fowler/Noll/Vo string hash

---
+++ fpsgame/gui/fnv_hash.cpp
@@ -0,0 +1,133 @@
+/* Copyright (c) 2010 Wildfire Games
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * Fowler/Noll/Vo string hash
+ */
+
+#include "precompiled.h"
+
+
+// FNV1-A hash - good for strings.
+// if len = 0 (default), treat buf as a C-string;
+// otherwise, hash <len> bytes of buf.
+u32 fnv_hash(const void* buf, size_t len)
+{
+	u32 h = 0x811c9dc5u;
+	// give distinct values for different length 0 buffers.
+	// value taken from FNV; it has no special significance.
+
+	const u8* p = (const u8*)buf;
+
+	// expected case: string
+	if(!len)
+	{
+		while(*p)
+		{
+			h ^= *p++;
+			h *= 0x01000193u;
+		}
+	}
+	else
+	{
+		size_t bytes_left = len;
+		while(bytes_left != 0)
+		{
+			h ^= *p++;
+			h *= 0x01000193u;
+
+			bytes_left--;
+		}
+	}
+
+	return h;
+}
+
+
+// FNV1-A hash - good for strings.
+// if len = 0 (default), treat buf as a C-string;
+// otherwise, hash <len> bytes of buf.
+u64 fnv_hash64(const void* buf, size_t len)
+{
+	u64 h = 0xCBF29CE484222325ull;
+	// give distinct values for different length 0 buffers.
+	// value taken from FNV; it has no special significance.
+
+	const u8* p = (const u8*)buf;
+
+	// expected case: string
+	if(!len)
+	{
+		while(*p)
+		{
+			h ^= *p++;
+			h *= 0x100000001B3ull;
+		}
+	}
+	else
+	{
+		size_t bytes_left = len;
+		while(bytes_left != 0)
+		{
+			h ^= *p++;
+			h *= 0x100000001B3ull;
+
+			bytes_left--;
+		}
+	}
+
+	return h;
+}
+
+
+// special version for strings: first converts to lowercase
+// (useful for comparing mixed-case filenames).
+// note: still need <len>, e.g. to support non-0-terminated strings
+u32 fnv_lc_hash(const char* str, size_t len)
+{
+	u32 h = 0x811c9dc5u;
+	// give distinct values for different length 0 buffers.
+	// value taken from FNV; it has no special significance.
+
+	// expected case: string
+	if(!len)
+	{
+		while(*str)
+		{
+			h ^= tolower(*str++);
+			h *= 0x01000193u;
+		}
+	}
+	else
+	{
+		size_t bytes_left = len;
+		while(bytes_left != 0)
+		{
+			h ^= tolower(*str++);
+			h *= 0x01000193u;
+
+			bytes_left--;
+		}
+	}
+
+	return h;
+}
---
+++ fpsgame/gui/fnv_hash.h
@@ -0,0 +1,55 @@
+/* Copyright (c) 2010 Wildfire Games
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * Fowler/Noll/Vo string hash
+ */
+
+#ifndef INCLUDED_FNV_HASH
+#define INCLUDED_FNV_HASH
+
+/**
+ * rationale: this algorithm was chosen because it delivers 'good' results
+ * for string data and is relatively simple. other good alternatives exist;
+ * see Ozan Yigit's hash roundup.
+ **/
+
+/**
+ * calculate FNV1-A hash.
+ *
+ * @param buf input buffer.
+ * @param len if 0 (default), treat buf as a C-string; otherwise,
+ * indicates how many bytes of buffer to hash.
+ * @return hash result. note: results are distinct for buffers containing
+ * differing amounts of zero bytes because the hash value is seeded.
+ **/
+extern u32 fnv_hash(const void* buf, size_t len = 0);
+/// 64-bit version of fnv_hash.
+extern u64 fnv_hash64(const void* buf, size_t len = 0);
+
+/**
+ * special version of fnv_hash for strings: first converts to lowercase
+ * (useful for comparing mixed-case filenames)
+ **/
+extern u32 fnv_lc_hash(const char* str, size_t len = 0);
+
+#endif	// #ifndef INCLUDED_FNV_HASH
